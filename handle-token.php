a8a0e4d8bca95a305ef86101dadc18007d4e053a
David Faloye <davidfaloye9@gmail.com>
2023-01-21 12:37:49 +0100

    Update handle-token.php

--- handle-token.php
+++ handle-token.php
@@ -3,7 +3,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     //require database and userid
     $token=stripslashes(htmlspecialchars(trim($_POST["token"])));//receive unique device token
 
-    $res=set_token($userid,$token);//store token for a particular
+    $res=set_token($userid,$token);//store token for a particular user in your database
     
     $arr["res"]=$res;
     echo json_encode($arr);//send feedback
